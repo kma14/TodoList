a6fad2efc678b055ce587925ea09b339f4919e0d
PNZAD\kevin <kevin@pnzad.technology>
2023-07-17 14:34:54 +1200

    Start point

---
+++ Backend/TodoList.Api/TodoList.Domain/ListItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Domain
+{
+    internal class ListItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ListItem(string name, string description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            IsCompleted = false;
+        }
+    }
+}
